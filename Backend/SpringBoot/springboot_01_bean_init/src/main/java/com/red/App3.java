f96b4b7d294ff2038786965615450510bb7c0894
redyouzi <1589029587@qq.com>
2023-03-02 19:43:43 +0800

    update

--- Backend/SpringBoot/springboot_01_bean_init/src/main/java/com/red/App3.java
+++ Backend/SpringBoot/springboot_01_bean_init/src/main/java/com/red/App3.java
@@ -7,7 +7,13 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 public class App3 {
     public static void main(String[] args) throws Exception {
         AnnotationConfigApplicationContext annotationConfigApplicationContext = new AnnotationConfigApplicationContext(SpringConfig.class);
-        Dog dog = annotationConfigApplicationContext.getBean("dog", Dog.class);
-        System.out.println(dog);
+//        Dog dog = annotationConfigApplicationContext.getBean("dog", Dog.class);
+//        System.out.println(dog);
+        for (String beanDefinitionName : annotationConfigApplicationContext.getBeanDefinitionNames()) {
+            System.out.println(beanDefinitionName);
+        }
+        SpringConfig springConfig = annotationConfigApplicationContext.getBean("springConfig", SpringConfig.class);
+        System.out.println(springConfig);
+
     }
 }
--- Backend/SpringBoot/springboot_01_bean_init/src/main/java/com/red/config/SpringConfig.java
+++ Backend/SpringBoot/springboot_01_bean_init/src/main/java/com/red/config/SpringConfig.java
@@ -1,19 +1,25 @@
 package com.red.config;
 
+import com.red.entity.Cat;
 import com.red.entity.DogFactoryBean;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.*;
 
-@Configuration
+//@Configuration(proxyBeanMethods = false)//会重新得到一个对象
+//所以使用Configuration注解的类本质上是一个配置类
+@Configuration(proxyBeanMethods = true)//执行cat方法会从容器中获取bean不是重新创建
 @ComponentScan("com.red")
 @Import({JdbcConfig.class})
+//配置类中引入原始的xml的方式的配置文件
+@ImportResource("classpath:applicationContext.xml")
 public class SpringConfig{
-
 
     @Bean
     public DogFactoryBean dog(){
         return new DogFactoryBean();
+    }
+
+    @Bean
+    public Cat cat(){
+        return new Cat();
     }
 }
