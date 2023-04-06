4db8c5919e61c1b3e01387c6a44bd4cf3d4fef02
humeng <humeng_mail@163.com>
2023-04-06 09:38:46 +0800

    update

--- Backend/Redis/LoginTest/src/components/common/LoginCard.vue
+++ Backend/Redis/LoginTest/src/components/common/LoginCard.vue
@@ -1,31 +1,5 @@
-<script setup>
+<script setup lang="ts">
 let time = new Date();
-
-setInterval(() => {
-    let date = new Date()
-    let year = date.getFullYear()
-    let month = date.getMonth() + 1
-    let day = date.getDate()
-    let hour = date.getHours()
-    let minute = date.getMinutes()
-    let second = date.getSeconds()
-    if (month < 10) {
-        month = '0' + month
-    }
-    if (day < 10) {
-        day = '0' + day
-    }
-    if (hour < 10) {
-        hour = '0' + hour
-    }
-    if (minute < 10) {
-        minute = '0' + minute
-    }
-    if (second < 10) {
-        second = '0' + second
-    }
-    time.innerHTML = `${year}年${month}月${day}日 ${hour}:${minute}:${second}`
-}, 16)
 
 </script>
 
